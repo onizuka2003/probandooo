575e4eead21d545efa6b3693a7c4c393b4131238
vanegas <avanegas@endeporte.edu.co>
2026-02-28 21:38:08 -0500

    xd

---
+++ express/backend.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql2/promise');
+var cors = require('cors')
+const session = require('express-session')
+
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:5173'
+}))
+app.use(session({
+  secret: 'mi-secreto',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 60000 } // 1 minuto de duración
+  
+  
+}))
+
+// ✅ Pool de conexiones (mejor que "connection" suelto)
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'login',
+});
+
+//__________________________________________________________________
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/login', async (req, res) => {
+  const datos = req.query;
+
+  try {
+    const [results] = await connection.query(
+      "SELECT * FROM `usuarios` WHERE `usuario` = ? AND `clave` = ?",
+      [datos.usuario, datos.clave]
+    );
+
+    
+    if (results.length > 0) {
+      req.session.usuario = datos.usuario; // Guardar el usuario en la sesión
+      return res.status(200).send({ ok: true, message: 'Inicio de sesión correcto' });
+    } else {
+      return res.status(401).send({ ok: false, message: 'Usuario o clave incorrectos' });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ ok: false, message: 'Error del servidor' });
+  }
+});
+
+app.get('/validate', (req, res) => {
+  if (req.session.usuario) { 
+    return res.status(200).send("sesion valida" );
+  }else{  
+  return res.status(401).send("sesion no valida");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
